9860ee8971b194b3050c348fc10f169fd5d57cb3
Oneill38 <megan.oneill38@gmail.com>
2014-04-01 16:39:13 -0400

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ JamOutApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   root 'search#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
